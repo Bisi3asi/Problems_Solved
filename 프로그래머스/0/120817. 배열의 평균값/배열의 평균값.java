3d3197f817976829222cfda64589ef104986191f
Bi3a <60452780+Bisi3asi@users.noreply.github.com>
2024-05-15 23:54:20 +0900

    [level 0] Title: 배열의 평균값, Time: 0.03 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double solution(int[] numbers) {
+        int sum = 0;
+        for (int i : numbers)
+            sum += i;
+       
+        return (double)sum / numbers.length;
+    }
+}
