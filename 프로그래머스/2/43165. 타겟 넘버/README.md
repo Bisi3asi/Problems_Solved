2ebcfeb0737d035ed014e90298b0dc07e4f57c74
Bi3a <60452780+Bisi3asi@users.noreply.github.com>
2025-02-02 22:58:58 +0900

    [level 2] Title: 타겟 넘버, Time: 25.12 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 78.5 MB, 시간: 0.67 ms
+메모리: 74.1 MB, 시간: 25.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 30일 23:33:26
+2025년 02월 02일 22:58:53
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,25 +1,23 @@
-class Solution {
-    public int solution(int[] numbers, int target) {
-        return new NumberOfCases(numbers, target).calc();
-    }
-}
-
-class NumberOfCases {
+class DFS {
+    private int target; 
     private int[] numbers;
-    private int target;
+    public int sum;
     
-    public NumberOfCases(int[] numbers, int target) {
+    public DFS(int[] numbers, int target) {
         this.numbers = numbers;
         this.target = target;
-    }
-    
-    public int calc() {
-        return calc(0, 0);
+        this.sum = 0;
     }
     
     public int calc(int depth, int sum) {
-        if ( depth == numbers.length ) return sum == target ? 1 : 0;
-        
+        if (depth == numbers.length) return sum == target ? 1 : 0;
         return calc(depth + 1, sum + numbers[depth]) + calc(depth + 1, sum - numbers[depth]);
-    }   
+    }
+}
+
+class Solution {
+    public int solution(int[] numbers, int target) {
+        DFS dfs = new DFS(numbers, target);
+        return dfs.calc(0, 0);
+    }
 }
