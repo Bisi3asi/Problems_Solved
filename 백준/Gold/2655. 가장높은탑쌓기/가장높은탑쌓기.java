f60a7a25329ae8bd4bc668284b88e046ca1f2c67
Sangje Lee <kueeng8008@hufs.ac.kr>
2026-03-29 22:06:42 +0900

    [Gold III] Title: 가장높은탑쌓기, Time: 196 ms, Memory: 18024 KB -BaekjoonHub

--- 백준/Gold/2655. 가장높은탑쌓기/가장높은탑쌓기.java
+++ 백준/Gold/2655. 가장높은탑쌓기/가장높은탑쌓기.java
@@ -1,88 +1,80 @@
 import java.util.*;
 
-public class Main {
-    static int[] dp;
-    static int[] parent;
+class BBrick implements Comparable<BBrick> {
+	int area, height, weight;
+	int originalIndex; // 입력 순서
 
-    private Queue<Integer> solution (List<Brick> bricks, int n) {
+	BBrick(int area, int height, int weight, int originalIndex) {
+		this.area = area;
+		this.height = height;
+		this.weight = weight;
+		this.originalIndex = originalIndex;
+	}
 
-        // dp[i] = i 번째 블록을 맨 위에 쌓았을 때 최대 총 높이
-        dp = new int[n];
-        parent = new int[n];
-        Arrays.fill(parent, -1);
-
-        for (int i = 0; i < n; i++) {
-            dp[i] = bricks.get(i).h;
-
-            for (int j = 0; j < i; j++) {
-                // 정렬 기준이 밑면이므로 밑면은 배제, 무게만 본다.
-                if (bricks.get(i).w < bricks.get(j).w) {
-                    if (dp[i] < dp[j] + bricks.get(i).h) {
-                        dp[i] = dp[j] + bricks.get(i).h;
-                        parent[i] = j;
-                    }
-                }
-            }
-        }
-
-        int maxHeight = 0;
-        int answerIndex = 0;
-        for (int i = 0; i < n; i++) {
-            if (dp[i] > maxHeight) {
-                maxHeight = dp[i];
-                answerIndex = i;
-            }
-        }
-
-        Queue<Integer> q = new LinkedList<>();
-        int cur = answerIndex;
-        while (cur != -1) {
-            q.offer(bricks.get(cur).index + 1);
-            cur = parent[cur];
-        }
-        return q;
-    }
-
-    public static void main(String[] args) {
-        Main T = new Main();
-        Scanner sc = new Scanner(System.in);
-        List<Brick> bricks = new ArrayList<>();
-
-        int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            int b = sc.nextInt();
-            int h = sc.nextInt();
-            int w = sc.nextInt();
-            bricks.add(new Brick(b, h, w, i));
-        }
-
-        Collections.sort(bricks);
-        Queue<Integer> answer = T.solution(bricks, n);
-
-        System.out.println(answer.size());
-        while(!answer.isEmpty()) System.out.println(answer.poll());
-
-        sc.close();
-    }
+	@Override
+	public int compareTo(BBrick o) {
+		return o.area - this.area; // 내림차순: 밑면 넓이 큰 것 먼저
+	}
 }
 
+public class Main {
+	static int[] dy;      // 최대 높이 저장
+	static int[] path;    // 경로 추적용
 
-class Brick implements Comparable<Brick> {
-    int b;
-    int h;
-    int w;
-    int index;
+	public static List<Integer> solution(List<BBrick> arr) {
+		int n = arr.size();
+		dy = new int[n];
+		path = new int[n];
+		Arrays.fill(path, -1);
 
-    Brick(int b, int h, int w, int i) {
-        this.b = b;
-        this.h = h;
-        this.w = w;
-        this.index = i;
-    }
+		Collections.sort(arr); // 내림차순 정렬
 
-    // 밑면은 비교대상에서 배제
-    @Override
-    public int compareTo(Brick o) {
-        return o.b - this.b;
-    }
+		int maxIndex = 0;
+
+		for (int i = 0; i < n; i++) {
+			dy[i] = arr.get(i).height;
+			for (int j = 0; j < i; j++) {
+				// i 위에 j 블록 올릴 수 있는지 확인 (ArrayList 접근)
+				if (arr.get(j).area > arr.get(i).area && arr.get(j).weight > arr.get(i).weight) {
+					if (dy[i] < dy[j] + arr.get(i).height) {
+						dy[i] = dy[j] + arr.get(i).height;
+						path[i] = j;
+					}
+				}
+			}
+			if (dy[i] > dy[maxIndex]) maxIndex = i;
+		}
+
+		// 경로 추적: 위 -> 아래
+		List<Integer> result = new ArrayList<>();
+		int idx = maxIndex;
+		while (idx != -1) {
+			result.add(arr.get(idx).originalIndex);
+			idx = path[idx];
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		List<BBrick> arr = new ArrayList<>();
+
+		for (int i = 0; i < n; i++) {
+			int area = sc.nextInt();
+			int height = sc.nextInt();
+			int weight = sc.nextInt();
+			arr.add(new BBrick(area, height, weight, i + 1)); // 1-based 번호
+		}
+
+		List<Integer> tower = solution(arr);
+
+		// 출력
+		System.out.println(tower.size()); // 블록 수
+		for (int num : tower) {           // 위 -> 아래 순서
+			System.out.println(num);
+		}
+
+		sc.close();
+	}
 }
