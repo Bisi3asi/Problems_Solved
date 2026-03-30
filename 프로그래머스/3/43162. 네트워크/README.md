61036f57341cc4572e6d580078f01cc334b3f5a9
Sangje Lee <kueeng8008@hufs.ac.kr>
2026-03-31 01:22:38 +0900

    [level 3] Title: 네트워크, Time: 0.59 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 83.2 MB, 시간: 0.67 ms
+메모리: 93.4 MB, 시간: 0.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 09일 19:31:22
+2026년 03월 31일 01:22:34
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,21 +1,26 @@
 import java.util.*;
 
+// 프로그래머스 - 네트워크
+// 연결된 컴퓨터 그룹(네트워크) 개수 찾기
+// 핵심: DFS로 연결된 모든 컴퓨터 방문 → 한 번의 DFS = 네트워크 1개
 class DFS {
-    private int[][] computers;
-    public boolean[] visited;
-    
+    private int[][] computers; // 인접 행렬
+    public boolean[] visited;  // 방문 여부
+
     DFS(int[][] computers, int n) {
         this.computers = computers;
         this.visited = new boolean[n];
     }
 
-    public void calc(int target, int n){
-        visited[target] = true;
-        
+    // target에서 시작해서 연결된 모든 컴퓨터 방문
+    public void calc(int target, int n) {
+        visited[target] = true; // 현재 컴퓨터 방문 처리
+
         for (int i = 0; i < n; i++) {
-            if (i == target) continue;
+            if (i == target) continue; // 자기 자신 제외
+            // 아직 방문 안 했고 연결되어 있으면 재귀 탐색
             if (!visited[i] && computers[target][i] == 1) calc(i, n);
-        } 
+        }
     }
 }
 
@@ -23,14 +28,15 @@ class Solution {
     public int solution(int n, int[][] computers) {
         DFS dfs = new DFS(computers, n);
         int answer = 0;
-        
+
         for (int i = 0; i < n; i++) {
+            // 아직 방문 안 한 컴퓨터 = 새로운 네트워크 발견
             if (!dfs.visited[i]) {
-                dfs.calc(i, n);
-                answer++;
+                dfs.calc(i, n); // 연결된 모든 컴퓨터 방문
+                answer++;       // 네트워크 카운트
             }
         }
-        
+
         return answer;
     }
 }
